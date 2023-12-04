4c6fd37a0a2d298cd3a4211aaef04984118ff806
Sean P Kelly <cbgbt@cabbage.bot>
2023-12-03 20:44:19 -0800

    add template solution

---
+++ src/tmpl/main.rs
@@ -0,0 +1,36 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+const INPUT: &str = include_str!("input.dat");
+
+lazy_static! {
+    static ref EXAMPLE: Regex = Regex::new(r"example").unwrap();
+}
+
+mod pt1 {
+    use super::*;
+
+    pub fn pt1() {
+        let result: u32 = 0;
+
+        println!("{}", result);
+    }
+}
+
+mod pt2 {
+    use super::*;
+
+    pub fn pt2() {
+        let result: u32 = 0;
+
+        println!("{}", result);
+    }
+}
+
+fn main() {
+    println!("Part 1:");
+    pt1::pt1();
+    println!("Part 2:");
+    pt2::pt2();
+}
